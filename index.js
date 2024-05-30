5ce64c809e87bb6bba49af26bf3ab028b36abc72
redwolf-danger <soniadvaita@gmail.com>
2024-05-30 18:20:09 +0530

    modified index.js to also use qs and axios

---
+++ index.js
@@ -0,0 +1,52 @@
+
+import qs from 'https://cdn.jsdelivr.net/npm/qs@6.12.1/+esm';
+import axios from 'https://cdn.jsdelivr.net/npm/axios@1.7.2/+esm'
+
+console.log('inside index.js');
+console.log(document.getElementById("btn"));
+let btn = document.getElementById("btn");
+
+console.log('btn is ',btn);
+
+function handler(){
+    console.log('button clicked');
+    
+    var data = qs.stringify({
+        'code': 'val = int(input("Enter your value: ")) + 5\nprint(val)',
+        'language': 'py',
+        'input': '7'
+    });
+
+    var config = {
+        method: 'post',
+        url: 'https://api.codex.jaagrav.in',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data : data
+    };
+    
+    
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+}
+
+btn.addEventListener("click",handler);
+
+
+
+// function handler(){
+//     console.log('button clicked');
+    
+// var data = qs.stringify({
+//     'code': 'val = int(input("Enter your value: ")) + 5\nprint(val)',
+//     'language': 'py',
+//     'input': '7'
+// });
+// console.log('data is ',data);
+// };
